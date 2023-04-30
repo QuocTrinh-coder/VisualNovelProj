21f240408ce3527ab949a7c9a7162467e265ac36
QuocTrinh-coder <harrytrinh2004@gmail.com>
2023-04-30 04:19:23 -0700

    added switch scene

--- VisualNovel/Assets/Script/SceneSwitcher.cs
+++ VisualNovel/Assets/Script/SceneSwitcher.cs
@@ -6,7 +6,6 @@ public class SceneSwitcher : MonoBehaviour
 {
     public void playGame()
     {
-        Debug.Log("hi");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
